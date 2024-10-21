6516ccaedb80f44d5b621fdaa03c24bb74ff9b50
Myst <yoannyo77@gmail.com>
2024-10-21 18:11:41 +0200

    Commercant fait

---
+++ src/Personnages/Commercant.java
@@ -0,0 +1,22 @@
+package Personnages;
+
+public class Commercant extends Humain {
+	// super = la classe mere
+	public Commercant(String nom, int argent) {
+		super(nom, "the", argent);
+	}
+	
+	
+	public int seFaireExtorquer() {
+		this.perdreArgent(this.getArgent() - this.getArgent());
+		this.parler("J'ai tout perdu! Le monde est trop injuste...");
+		return 0;
+	}
+	
+	public void recevoir(int argent) {
+		this.gagnerArgent(argent);
+		this.parler(argent + " sous! Je te remercie genereux donateur.");
+		return;
+	}
+
+}
